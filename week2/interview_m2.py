0ab6506d389e1f049a8ed87694169b6d45ea6dda
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-08-09 09:24:07 -0500

    Create new files and classes for Deque and Permutation, rename interview_m2.py to RandomizedQueue.java, and add package statements to Percolation.java and PercolationStats.java in week1/hw1

---
+++ week2/interview_m2.py
@@ -0,0 +1,31 @@
+# Implement a queue with two stacks so that each queue op takes a
+# constant amortized number of stack operations
+
+from typing import Item
+
+'''
+A queue works by having a queue op inserting at the left
+and a deque by removing from the right.
+
+
+'''
+
+class Queue:
+
+
+    def __init__(self) -> None:
+        stack1 = []
+        stack2 = []
+
+    def enqueue(item: Item) -> None:
+        pass
+
+    def dequeue() -> Item:
+        pass
+
+    def size() -> int:
+        pass
+
+
+
+
