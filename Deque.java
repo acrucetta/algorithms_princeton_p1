055b5eb7f83bca8d722c20746e23053a856c9382
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-08-10 09:48:56 -0500

    Refactor Deque and RandomizedQueue classes to implement Iterable interface

--- Deque.java
+++ Deque.java
@@ -1,3 +1,72 @@
-public class Deque {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+
+  private Node first;
+  private Node last;
+  private int N = 0;
+
+  private class Node {
+    Item item;
+    Node next;
+  }
+
+  // construct an empty deque (constant time (O(1)))
+  public Deque() {
+
+  }
+
+  // is the deque empty?
+  public boolean isEmpty() {
+    return N == 0;
+  }
+
+  // return the number of items on the deque
+  public int size() {
+    return N;
+  }
+
+  // add the item to the front (constant)
+  public void addFirst(Item item) {
+    if (item == null) {
+      throw new IllegalArgumentException("item cannot be null");
+    }
+    Node oldfirst = first;
+    first = new Node();
+    first.item = item;
+    first.next = oldfirst;
+  }
+
+  // add the item to the back (constant)
+  public void addLast(Item item) {
+    if (item == null) {
+      throw new IllegalArgumentException("item cannot be null");
+    }
+
+  }
+
+  // remove and return the item from the front (constant)
+  public Item removeFirst() {
+    if (isEmpty())
+      throw new NoSuchElementException("deque is empty");
+  }
+
+  // remove and return the item from the back (constant)
+  public Item removeLast() {
+    if (isEmpty())
+      throw new NoSuchElementException("deque is empty");
+  }
+
+  // return an iterator over items in order from front to back
+  public Iterator<Item> iterator() {
+    if (isEmpty())
+      throw new NoSuchElementException("deque is empty");
+  }
+
+  // unit testing (required)
+  public static void main(String[] args) {
+    return;
+  }
 
 }
