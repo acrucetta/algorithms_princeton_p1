bf753e2d17bb54d43413bf7f98b9c2ad58d46518
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-08-25 09:46:58 -0500

    Add support for launching the FastCollinearPoints class in the launch.json configuration file

--- homeworks/hw3/FastCollinearPoints.java
+++ homeworks/hw3/FastCollinearPoints.java
@@ -4,12 +4,60 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class FastCollinearPoints {
-  public FastCollinearPoints(Point[] points)     // finds all line segments containing 4 or more points
 
-  public           int numberOfSegments()        // the number of line segments
+  private final List<LineSegment> lineSegments = new ArrayList<>();
 
-  public LineSegment[] segments()                // the line segments
+  public FastCollinearPoints(Point[] points) // finds all line segments containing 4 or more points
+  {
+    if (points == null) {
+      throw new IllegalArgumentException();
+    }
+    for (Point origin : points) {
+      if (origin == null) {
+        throw new IllegalArgumentException();
+      }
+      // We think of P as the origin.
+      // For each other point q, determine the slope it makes with p.
+      HashMap<Double, List<Point>> slopes = new HashMap<>();
+      for (Point q : points) {
+        if (q == null || q == origin) {
+          continue;
+        }
+        double slope = origin.slopeTo(q);
+        slopes.computeIfAbsent(slope, k -> new ArrayList<>()).add(q);
+      }
+      // Check if any slope has more than 3 points.
+      for (Map.Entry<Double, List<Point>> entry : slopes.entrySet()) {
+        List<Point> collinearPoints = entry.getValue();
+        if (collinearPoints.size() >= 3) {
+          collinearPoints.add(origin);
+          collinearPoints.sort(Point::compareTo);
+          Point min = collinearPoints.get(0);
+          Point max = collinearPoints.get(collinearPoints.size() - 1);
+          if (min == origin) {
+            lineSegments.add(new LineSegment(min, max));
+          }
+        }
+      }
+    }
+  }
+
+  public int numberOfSegments() // the number of line segments
+  {
+    return lineSegments.size();
+  }
+
+  public LineSegment[] segments() // the line segments
+  {
+    return lineSegments.toArray(new LineSegment[0]);
+  }
 
   public static void main(String[] args) {
 
