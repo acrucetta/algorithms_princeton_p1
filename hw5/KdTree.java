7e7f26978e09dcaa25023a8cc2537e21d8e8f37c
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-10-25 09:57:20 -0500

    added insert to kdtree class

--- hw5/KdTree.java
+++ hw5/KdTree.java
@@ -4,26 +4,78 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
+    private Node root;
 
-    private class Node {
-        Point2D point;
-        Node left;
-        Node right;
-        boolean isVertical;
-        RectHV rect;
+    public static class Node {
+        public Point2D point;
+        public boolean isVertical;
+        public Node left;
+        public Node right;
+
+        public Node(Point2D point) {
+            this.point = point;
+            this.isVertical = true;
+            this.left = null;
+            this.right = null;
+        }
     }
 
     public KdTree() {
-        Node root = null;
-        root.isVertical = true;
-        root.left = null;
-        root.right = null;
-        root.rect = new RectHV(0, 0, 1, 1);
+        this.root = new Node(null);
     }
     public void insert(Point2D p) {
+        // We want to start from the root. We compare the x-coordinate (if the point to be inserted has a smaller x-coordinate
+        // than the point at the root, go left; otherwise go right); then, we compare the y-coordinate (if the point to be inserted
+        // has a smaller y-coordinate than the point in the node, go left; otherwise go right). Once we reach a null link,
+        // we insert the new node there.
+        Node newNode = new Node(p);
+        newNode.point = p;
+
+        if (root == null) {
+            root = newNode;
+        }
+        Node current = root;
+        while (current.point != null) {
+            if (current.isVertical) {
+                if (p.x() < current.point.x()) {
+                    if (current.left == null) {
+                        current.left = newNode;
+                        break;
+                    } else {
+                        current = current.left;
+                    }
+                } else {
+                    if (current.right == null) {
+                        current.right = newNode;
+                        break;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            } else {
+                if (p.y() < current.point.y()) {
+                    if (current.left == null) {
+                        current.left = newNode;
+                        break;
+                    } else {
+                        current = current.left;
+                    }
+                } else {
+                    if (current.right == null) {
+                        current.right = newNode;
+                        break;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
     }
 
     public void draw() {
+        // All points to the left of the root go in the left subtree; all points to the right go in the right subtree.
+        // The vertical splits are red and the horizontal splits are blue.
+        return;
     }
 
     public KdTree nearest(Point2D query) {
