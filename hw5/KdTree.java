f252edca635a65006aabd469ed4d884393cf4161
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-10-26 09:49:24 -0500

    created draw method

--- hw5/KdTree.java
+++ hw5/KdTree.java
@@ -1,6 +1,7 @@
 package homeworks.hw5;
 
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
@@ -74,8 +75,25 @@ public class KdTree {
 
     public void draw() {
         // All points to the left of the root go in the left subtree; all points to the right go in the right subtree.
-        // The vertical splits are red and the horizontal splits are blue.
-        return;
+        // The vertical splits are red and the horizontal splits are blue.; we can use BFS to traverse the tree.
+        Queue<Node> queue = new Queue<>();
+        queue.enqueue(root);
+        while (!queue.isEmpty()) {
+            Node current = queue.dequeue();
+            if (current.isVertical) {
+                // Draw a vertical line from the point to the left of the node to the point to the right of the node.
+                current.point.drawTo(new Point2D(current.point.x(), current.point.y() + 1));
+            } else {
+                // Draw a horizontal line from the point below the node to the point above the node.
+                current.point.drawTo(new Point2D(current.point.x() + 1, current.point.y()));
+            }
+            if (current.left != null) {
+                queue.enqueue(current.left);
+            }
+            if (current.right != null) {
+                queue.enqueue(current.right);
+            }
+        }
     }
 
     public KdTree nearest(Point2D query) {
@@ -84,5 +102,18 @@ public class KdTree {
 
     public Point2D[] range(RectHV rect) {
         return null;
+    }
+
+    public static void main(String[] args) {
+        // Unit testing of the methods (not graded).
+        // Create a KdTree object and insert some points.
+        KdTree kdTree = new KdTree();
+        kdTree.insert(new Point2D(0.7, 0.2));
+        kdTree.insert(new Point2D(0.5, 0.4));
+        kdTree.insert(new Point2D(0.2, 0.3));
+        kdTree.insert(new Point2D(0.4, 0.7));
+        kdTree.insert(new Point2D(0.9, 0.6));
+        // Draw the points to the standard draw.
+        kdTree.draw();
     }
 }
