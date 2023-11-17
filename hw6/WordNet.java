373e48be672ac57c6a6a2af3d457551f51b5aae1
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-11-17 10:07:39 -0600

    Refactor WordNet constructor and initialize variables

--- hw6/WordNet.java
+++ hw6/WordNet.java
@@ -11,17 +11,13 @@ import java.util.*;
 public class WordNet {
 
     private final Map<Integer,String> synsets = new TreeMap<>();
-    private final Map<String, Set<Integer>> WordToIds = new TreeMap<>();
+    private final Map<String, Integer> WordToIds = new TreeMap<>();
     private TreeSet<String> nouns;
-    private Digraph digraph;
     private Map<Integer, List<Integer>> hypernyms;
-    private SAP sap;
+    private final SAP sap;
 
     // constructor takes the name of the two input files (linearithmic or better)
     public WordNet(String synsets, String hypernyms) {
-        // We will first load the synsets file and build a map
-        // from synset id to synset.
-        // Then we will load the hypernyms file and build a digraph.
         try {
             Scanner scanner = new Scanner(new FileReader(synsets));
             while (scanner.hasNextLine()) {
@@ -29,8 +25,13 @@ public class WordNet {
                 String[] fields = line.split(",");
                 int id = Integer.parseInt(fields[0]);
                 String synset = fields[1];
+
+                // We will build a set of all nouns
                 this.synsets.put(id, synset);
                 this.nouns.add(synset);
+
+                // We will also build a map from each word to the set of synset ids
+                this.WordToIds.putIfAbsent(synset, id);
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -39,6 +40,7 @@ public class WordNet {
         // Now we will load the hypernyms file and build a digraph.
         // Each vertex is the id of a synset; each directed edge
         // represents that w is a hypernym of v.
+        Digraph digraph = new Digraph(0);
         try {
             Scanner scanner = new Scanner(new FileReader(hypernyms));
             while (scanner.hasNextLine()) {
@@ -47,14 +49,15 @@ public class WordNet {
                 int id = Integer.parseInt(fields[0]);
                 for (int i = 1; i < fields.length; i++) {
                     int hypernymId = Integer.parseInt(fields[i]);
-                    if (this.digraph != null) {
-                        this.digraph.addEdge(id, hypernymId);
-                    }
+                    digraph.addEdge(id, hypernymId);
                 }
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+
+        // Build the SAP object to compute shortest ancestral paths
+        sap = new SAP(digraph);
     }
 
     // returns all WordNet nouns
@@ -72,19 +75,22 @@ public class WordNet {
     public int distance(String nounA, String nounB) {
         // We will first find the synset ids of nounA and nounB.
         // Then we will find the shortest ancestral path between
-
+        int idNounA = this.WordToIds.get(nounA);
+        int idNounB = this.WordToIds.get(nounB);
+        return sap.length(idNounA, idNounB);
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below) (linear or better)
     // we want to do a BFS from nounA and nounB
     public String sap(String nounA, String nounB) {
-        return null;
+        int idNounA = this.WordToIds.get(nounA);
+        int idNounB = this.WordToIds.get(nounB);
+        int ancestorId = sap.ancestor(idNounA, idNounB);
+        return this.synsets.get(ancestorId);
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
-
-            return;
     }
 }
